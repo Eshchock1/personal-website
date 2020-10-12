2678e10850f0d3aa2fe893eed3e62bf08a9ed95d
Eshwar Chock <eshchock1@gmail.com>
2020-10-12 12:53:23 -0400

    responsive home

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,208 +1,239 @@
-import React, { Component } from 'react'
-import '../home.scss';
-import linkedin from '../linkedin.png';
-import instagram from '../instagram.png';
-import github from '../github.png';
-import ParallaxMousemove from 'react-parallax-mousemove'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-const style={
-    outter: {
-        background: '#e20025',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        width:'100%',
-        position: 'relative',
-        overflow: 'hidden',
-        display:'flex',
-      },
-    infoLayerStyle1: {
-        position: 'absolute',
-      },
-      infoLayerStyle2: {
-        position: 'absolute',
-      },
-      infoLayerStyle3: {
-        position: 'absolute',
-      },
-      infoLayerStyle4: {
-        position: 'absolute',
-      },
-      infoLayerStyle5: {
-        position: 'absolute',
-        
-      },
-      bruh1:{
-          position:'absolute',
-          top:'400px',
-          left:'200px',
-      },
-      bruh2:{
-        position:'absolute',
-        top:'500px',
-        left:'865px'
-      },
-      bruh3:{
-        position:'absolute',
-        top:'900px',
-        left:'1000px'
-      },
-      bruh4:{
-        position:'absolute',
-        top:'400px',
-        left:'1500px'
-      },
-      bruh5:{
-        position:'absolute',
-        top:'800px',
-        left:'1468px'
-      },
-      bruh6:{
-        position:'absolute',
-        top:'600px',
-        left:'400px'
-      }
-} 
- 
+import React, { Component } from "react";
+import "../home.scss";
+import linkedin from "../linkedin.png";
+import instagram from "../instagram.png";
+import github from "../github.png";
+import ParallaxMousemove from "react-parallax-mousemove";
+import HamburgerMenu from 'react-hamburger-menu';
+const style = {
+    layerStyle1: {
+        position: "absolute",
+    },
+    layerStyle2: {
+        position: "absolute",
+    },
+    layerStyle3: {
+        position: "absolute",
+    },
+    layerStyle4: {
+        position: "absolute",
+    },
+    layerStyle5: {
+        position: "absolute",
+    },
+    layerStyle6: {
+        position: "absolute",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+    },
+    ballContainer1: {
+        position: "absolute",
+        top: "37vh",
+        left: "10vw",
+    },
+    ballContainer2: {
+        position: "absolute",
+        top: "46vh",
+        left: "45vw",
+    },
+    ballContainer3: {
+        position: "absolute",
+        top: "83vh",
+        left: "52vw",
+    },
+    ballContainer4: {
+        position: "absolute",
+        top: "37vh",
+        left: "78vw",
+    },
+    ballContainer5: {
+        position: "absolute",
+        top: "74vh",
+        left: "76vw",
+    },
+    ballContainer6: {
+        position: "absolute",
+        top: "55vh",
+        left: "21vw",
+    },
+};
 
-function Home() {
+export default class Home extends React.Component {    
+  handleClick() {
+    this.setState({
+        open: !this.state.open
+    });
+}
+constructor(props) {
+  super(props);
+  this.state = { width: 0, height: 0, open:false};
+  this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+}
+
+componentDidMount() {
+  this.updateWindowDimensions();
+  window.addEventListener('resize', this.updateWindowDimensions);
+}
+
+componentWillUnmount() {
+  window.removeEventListener('resize', this.updateWindowDimensions);
+}
+
+updateWindowDimensions() {
+  this.setState({ width: window.innerWidth, height: window.innerHeight });
+}
+  render() {
+
+    
+  
   return (
-    <div>
-    <div className="container">
+        <div style={{width:"100vw", height:"100vh"}}>
+          <div className="mobileMenu">
+          <HamburgerMenu
+    isOpen={this.state.open}
+    menuClicked={this.handleClick.bind(this)}
+    width={this.state.width <= 991?0.075 * this.state.width:this.state.width <= 991? 0.06 * this.state.width:this.state.width <= 1366? 0.05 * this.state.width:0}
+    height={this.state.width <= 991? 0.05 * this.state.width:this.state.width <= 1366? 0.04 * this.state.width:0}
+    strokeWidth={this.state.width <= 991? 0.006 * this.state.width:this.state.width <= 1366? 0.005 * this.state.width:0}
+    rotate={0}
+    color='white'
+    borderRadius={5}
+    animationDuration={0.5}
+/>
+          </div>
+            <div className="container">
+                <ParallaxMousemove>
+                    <ParallaxMousemove.Layer
+                        layerStyle={style.layerStyle6}
+                        config={{
+                            xFactor: -0.04,
+                            yFactor: 0.03,
+                            springSettings: {
+                                stiffness: 75,
+                                damping: 5,
+                            },}}>
+                        <div className="breh">
+                            <h1>Eshwara</h1>
+                            <h2>Chock</h2>
+                        </div>
+                    </ParallaxMousemove.Layer>
+                </ParallaxMousemove>
+            </div>
 
-  <ParallaxMousemove>
-  <ParallaxMousemove.Layer layerStyle={style.infoLayerStyle1} config={{
-  xFactor: -0.04,
-  yFactor: 0.03,
-  springSettings: {
-    stiffness: 75,
-    damping: 5
+            <div class="socials">
+                <ul>
+                    <li>
+                        <img src={linkedin} className="social" alt="logo" />
+                    </li>
+                    <li>
+                        <img src={github} className="social" alt="logo" />
+                    </li>
+                    <li>
+                        <img src={instagram} className="social" alt="logo" />
+                    </li>
+                </ul>
+            </div>
+
+            <div class="links">
+                <a href="#">ABOUT</a>
+
+                <a href="#">CONTACT</a>
+
+                <a href="#">PORTFOLIO</a>
+            </div>
+
+            <div className="container2">
+                <ParallaxMousemove>
+                    <div style={style.ballContainer3}>
+                        <ParallaxMousemove.Layer
+                            layerStyle={style.layerStyle3}
+                            config={{
+                                xFactor: 0.06,
+                                yFactor: -0.1,
+                                springSettings: {
+                                    stiffness: 100,
+                                    damping: 5,
+                                },}}>
+                            <div className="ball3"></div>
+                        </ParallaxMousemove.Layer>
+                    </div>
+                    <div style={style.ballContainer4}>
+                        <ParallaxMousemove.Layer
+                            layerStyle={style.layerStyle4}
+                            config={{
+                                xFactor: 0.1,
+                                yFactor: -0.13,
+                                springSettings: {
+                                    stiffness: 75,
+                                    damping: 5,
+                                },}}>
+                            <div className="ball4"></div>
+                        </ParallaxMousemove.Layer>
+                    </div>
+                    <div style={style.ballContainer1}>
+                        <ParallaxMousemove.Layer
+                            layerStyle={style.layerStyle1}
+                            config={{
+                                xFactor: 0.1,
+                                yFactor: -0.13,
+                                springSettings: {
+                                    stiffness: 75,
+                                    damping: 5,
+                                },}}>
+                            <div className="ball1"></div>
+                        </ParallaxMousemove.Layer>
+                    </div>
+                </ParallaxMousemove>
+            </div>
+            <div className="container3">
+                <ParallaxMousemove>
+                    <div style={style.ballContainer6}>
+                        <ParallaxMousemove.Layer
+                            layerStyle={style.layerStyle5}
+                            config={{
+                                xFactor: -0.1,
+                                yFactor: 0.1,
+                                springSettings: {
+                                    stiffness: 75,
+                                    damping: 5,
+                                },}}>
+                            <div className="ball6"></div>
+                        </ParallaxMousemove.Layer>
+                    </div>
+
+                    <div style={style.ballContainer2}>
+                        <ParallaxMousemove.Layer
+                            layerStyle={style.layerStyle2}
+                            config={{
+                                xFactor: -0.16,
+                                yFactor: 0.2,
+                                springSettings: {
+                                    stiffness: 75,
+                                    damping: 5,
+                                },}}>
+                            <div className="ball2"></div>
+                        </ParallaxMousemove.Layer>
+                    </div>
+                    <div style={style.ballContainer5}>
+                        <ParallaxMousemove.Layer
+                            layerStyle={style.layerStyle5}
+                            config={{
+                                xFactor: -0.17,
+                                yFactor: 0.17,
+                                springSettings: {
+                                    stiffness: 75,
+                                    damping: 5,
+                                },}}>
+                            <div className="ball5"></div>
+                        </ParallaxMousemove.Layer>
+                    </div>
+                </ParallaxMousemove>
+            </div>
+        </div>
+    );
   }
-}}>
-<div className="breh">
-<h1>Eshwara</h1> 
-
-</div>
-</ParallaxMousemove.Layer>
-</ParallaxMousemove>
-</div>
-
-
-
-
-<div class="socials">
-  <ul>
-    <li><img src={linkedin} className="social" alt="logo" /></li>
-  <li><img src={github} className="social" alt="logo" /></li>
-  <li><img src={instagram} className="social" alt="logo" /></li>
-  </ul>
-</div>
-
-<div class="links">
-  <a href="#">ABOUT</a>
-
-  <a href="#">CONTACT</a>
-
-  <a href="#">PORTFOLIO</a>
-</div>
-
-
-
-
-<div className="container2">
-<ParallaxMousemove>
-
-<div style={style.bruh3}>
-
-<ParallaxMousemove.Layer layerStyle={style.infoLayerStyle3} config={{
-xFactor: 0.06,
-yFactor: -0.1,
-springSettings: {
-stiffness: 100,
-damping: 5
-}
-}}>
-<div className="ball3"></div>
-</ParallaxMousemove.Layer>
-</div>
-<div style={style.bruh4}>
-
-<ParallaxMousemove.Layer layerStyle={style.infoLayerStyle4} config={{
-xFactor: 0.1,
-yFactor: -0.13,
-springSettings: {
-stiffness: 75,
-damping: 5
-}
-}}>
-<div className="ball4"></div>
-</ParallaxMousemove.Layer></div>
-<div style={style.bruh1}>
-<ParallaxMousemove.Layer layerStyle={style.infoLayerStyle1} config={{
-  xFactor: 0.1,
-  yFactor: -0.13,
-  springSettings: {
-    stiffness: 75,
-    damping: 5
-  }
-}}>
-<div className="ball1"></div>
-</ParallaxMousemove.Layer></div>
-
-</ParallaxMousemove>
-</div>
-<div className="container3">
-
-<ParallaxMousemove>
-
-
-
-<div style={style.bruh6}>
-
-<ParallaxMousemove.Layer layerStyle={style.infoLayerStyle5} config={{
-xFactor: -0.1,
-yFactor: 0.1,
-springSettings: {
-stiffness: 75,
-damping: 5
-}
-}}>
-<div className="ball6"></div>
-</ParallaxMousemove.Layer>
-</div>
-
-
-<div style={style.bruh2}>
-
-<ParallaxMousemove.Layer layerStyle={style.infoLayerStyle2} config={{
-xFactor: -0.16,
-yFactor: 0.2,
-springSettings: {
-stiffness: 75,
-damping: 5
-}
-}}>
-<div className="ball2"></div>
-</ParallaxMousemove.Layer>
-</div>
-<div style={style.bruh5}>
-
-<ParallaxMousemove.Layer layerStyle={style.infoLayerStyle5} config={{
-xFactor: -0.17,
-yFactor: 0.17,
-springSettings: {
-stiffness: 75,
-damping: 5
-}
-}}>
-<div className="ball5"></div>
-</ParallaxMousemove.Layer>
-</div>
-
-</ParallaxMousemove>
-</div>
-
-</div>
-  );
 }
 
-export default Home;
