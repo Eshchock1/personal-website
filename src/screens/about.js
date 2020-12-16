0808801df431e22be6ab28874fdbfb3da39ed215
Eshwar Chock <eshchock1@gmail.com>
2020-12-15 20:21:18 -0500

    about page responsive

--- src/screens/about.js
+++ src/screens/about.js
@@ -12,14 +12,14 @@ constructor(props) {
   
   return (
 <div className="appContainer" id="work" style={{justifyContent:'flex-start'}}>
-    <div style={{display:'flex',flexDirection:'row', width:'100%', height:'100%'}}>
-      <div style={{display:'flex', flexDirection:'column', flex:0.5, height:'100%', alignItems:'flex-start', paddingLeft:"15%", justifyContent:'center', }}>
-        <h1 style={{color:'white', fontSize:'4vw', fontFamily:'edition', margin:0}}>HEY THERE<span className="noFont">👋!</span><br/>I<span className="noFont">'</span>M ESHWARA<span className="noFont">.</span></h1>
-        <h3 style={{color:'white', fontSize:'1.3vw', fontFamily:'Poppins', fontWeight:400, width:"80%", }}>A hands-on creative with more than 16 years design experience.</h3>
-        <p style={{color:'white', fontSize:'1vw', fontFamily:'Poppins', fontWeight:300, width:"80%", marginTop:"-0.5vw", }}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+    <div className="outerContainer">
+      <div className="textContainer">
+        <h1 className="title">HEY THERE<span className="noFont"><div className="animatedWave">👋</div>!</span><br/>I<span className="noFont">'</span>M ESHWARA<span className="noFont">.</span></h1>
+        <h3 className="subtitle">A hands-on creative with more than 16 years design experience.</h3>
+        <p className="content">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
       </div>
-      <div style={{display:'flex', flexDirection:'column', flex:0.5, height:'100%', alignItems:'flex-start', justifyContent:'center', }}>
-        <img style={{width:'40vw', height:'40vw', marginLeft:'-5vw'}} src={require('../emoji.png')}/>
+      <div className="emojiContainer">
+        <img className="emoji" src={require('../emoji.png')}/>
       </div>
     </div>
 
