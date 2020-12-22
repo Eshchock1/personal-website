1ac6d3734d61b333a92de2160e1a7ce5cbed2e97
Eshwar Chock <eshchock1@gmail.com>
2020-12-21 23:13:23 -0500

    changed all website content

--- src/screens/about.js
+++ src/screens/about.js
@@ -16,8 +16,8 @@ constructor(props) {
     <div className="outerContainer">
       <div className="textContainer">
         <h1 className="title">HEY THERE<span className="noFont"><div className="animatedWave">👋</div>!</span><br/>I<span className="noFont">'</span>M ESHWARA<span className="noFont">.</span></h1>
-        <h3 className="subtitle">A hands-on creative with more than 16 years design experience.</h3>
-        <p className="content">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+        <h3 className="subtitle">A 17 year old developer from Ottawa, Canada.</h3>
+        <p className="content">I’m currently working as a mobile engineer at SnapSmile and pursuing the IB Diploma Programme at Colonel By S.S in my senior year. Always exploring enriching opportunities with pioneering technology I can implement to change the world, I have a deep passion for STEM fields and computer science. In the past, I’ve worked with Digitera.Interactive, Ileap Club, 2381 Robotics, and Kilika Inc. In my spare time, you might find me playing badminton, coding, drawing, learning, and competing in hackathons. Feel free to take a look at some of my work on <a style={{color:'white', fontWeight:500, textDecoration:'none'}} href="http://github.com/Eshchock1">Github</a> or <a style={{color:'white', fontWeight:500, textDecoration:'none'}} href="mailto:eshchock1@gmail.com">shoot me a message</a>.</p>
       </div>
       <div className="emojiContainer">
       <ParallaxMousemove>
